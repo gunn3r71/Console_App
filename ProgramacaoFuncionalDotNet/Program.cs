3d4a2a07f505e82b81b43e30d95fe3875c4d4dfe
Lucas <lucasgunner3@gmail.com>
2021-04-21 19:41:28 -0300

    Documentando software

--- ProgramacaoFuncionalDotNet/Program.cs
+++ ProgramacaoFuncionalDotNet/Program.cs
@@ -6,57 +6,25 @@ namespace ProgramacaoFuncionalDotNet
 {
     class Program
     {
+        #region Definindo constantes de opções
         public const int SAIR = 0;
         public const int LER_ARQUIVOS = 1;
         public const int TABUADA = 2;
         public const int CALCULO_MEDIA = 3;
         public const int LIMPAR_TELA = 4;
+        #endregion
 
-        public static void ExibeTabuada(int n)
+        private static void ExibeMenu()
         {
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"{n} X {i} = {n*i}");
-            }
-        }
-
-        public static void LerArquivos(string nome, int numero_arquivo)
-        {
-            string nome_arquivo = $@"C:\ArquivosDotNetRead\{nome}{numero_arquivo}.txt"; // Monta a string que gera o caminho do arquivo a ser lido
-            if (File.Exists(nome_arquivo)) // verifica se arquivo existe
-            {
-                using (StreamReader arquivo = File.OpenText(nome_arquivo)) // Linha que abre o arquivo
-                {
-                    string linha;
-                    while ((linha = arquivo.ReadLine()) != null) // Código que faz a leitura das linhas
-                    {
-                        Console.WriteLine(linha);
-                    }
-                }
-                LerArquivos(nome, numero_arquivo + 1); // Chama a função incrementando um ao número do arquivo
-            }
-            else
-            {
-                Console.WriteLine("Não existem mais arquivos para Ler!");
-            }
-        }
-
-        public static void CalcularMedia(double nota1, double nota2, double nota3)
-        {
-            double media = (nota1 + nota2 + nota3) / 3;
-            Console.WriteLine($"Media calculada: {media.ToString("F2",CultureInfo.InvariantCulture)}");
-        }
-
-        static void Main(string[] args)
-        {
+            #region Exibe o Menu
             while (true)
             {
                 string menu = "Olá Usuário, seja bem vindo ao nosso programa!!!\n\n"
                     + "Agora digite uma das opções abaixo: \n\n"
                     + $"Para Ler Arquivos, Digite {LER_ARQUIVOS};\n"
-                    +$"Para Calcular uma tabuada, Digite {TABUADA};\n"
-                    +$"Para Efetuar um cálcuo de média, Digite {CALCULO_MEDIA};\n"
-                    +$"Para Limpar a tela, Digite {LIMPAR_TELA};\n\n";
+                    + $"Para Calcular uma tabuada, Digite {TABUADA};\n"
+                    + $"Para Efetuar um cálcuo de média, Digite {CALCULO_MEDIA};\n"
+                    + $"Para Limpar a tela, Digite {LIMPAR_TELA};\n\n";
 
                 Console.WriteLine($"{menu}Digite {SAIR} para sair do programa!");
                 Console.Write("> ");
@@ -76,7 +44,7 @@ namespace ProgramacaoFuncionalDotNet
                         string[] arquivo = Console.ReadLine().Split(",");
                         string nome_arquivo = arquivo[0];
                         int numero_arquivo = int.Parse(arquivo[1]);
-                        LerArquivos(nome_arquivo,numero_arquivo);
+                        LerArquivos(nome_arquivo, numero_arquivo);
                         Console.WriteLine("\n============================================\n");
                         break;
                     case TABUADA:
@@ -90,7 +58,7 @@ namespace ProgramacaoFuncionalDotNet
                         Console.WriteLine("\n================Calcular Média================\n");
                         Console.Write("Digite 3 notas separados por virgula (9.5,10.0,8.5): ");
                         string[] notas = Console.ReadLine().Split(",");
-                        CalcularMedia(double.Parse(notas[0],CultureInfo.InvariantCulture), double.Parse(notas[1], CultureInfo.InvariantCulture), double.Parse(notas[2], CultureInfo.InvariantCulture));
+                        CalcularMedia(double.Parse(notas[0], CultureInfo.InvariantCulture), double.Parse(notas[1], CultureInfo.InvariantCulture), double.Parse(notas[2], CultureInfo.InvariantCulture));
                         Console.WriteLine("\n==============================================\n");
                         break;
                     case LIMPAR_TELA:
@@ -103,6 +71,53 @@ namespace ProgramacaoFuncionalDotNet
                         break;
                 }
             }
+            #endregion
+        }
+
+        public static void ExibeTabuada(int n)
+        {
+            #region Exibindo Tabuada
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n*i}");
+            }
+            #endregion
+        }
+
+        public static void LerArquivos(string nome, int numero_arquivo)
+        {
+            #region Lendo arquivos
+            string nome_arquivo = $@"C:\ArquivosDotNetRead\{nome}{numero_arquivo}.txt"; // Monta a string que gera o caminho do arquivo a ser lido
+            if (File.Exists(nome_arquivo)) // verifica se arquivo existe
+            {
+                using (StreamReader arquivo = File.OpenText(nome_arquivo)) // Linha que abre o arquivo
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null) // Código que faz a leitura das linhas
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+                LerArquivos(nome, numero_arquivo + 1); // Chama a função incrementando um ao número do arquivo
+            }
+            else
+            {
+                Console.WriteLine("Não existem mais arquivos para Ler!");
+            }
+            #endregion
+        }
+
+        public static void CalcularMedia(double nota1, double nota2, double nota3)
+        {
+            #region Calcula e exibe média
+            double media = (nota1 + nota2 + nota3) / 3;
+            Console.WriteLine($"Media calculada: {media.ToString("F2",CultureInfo.InvariantCulture)}");
+            #endregion
+        }
+
+        static void Main(string[] args)
+        {
+            ExibeMenu();
         }
     }
 }
