de7038f5c4ef1dd8d742c166ddbb06f205c9bb57
Lucas <lucasgunner3@gmail.com>
2021-04-21 17:41:27 -0300

    Adicionar arquivos de projeto.

---
+++ ProgramacaoFuncionalDotNet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgramacaoFuncionalDotNet
+{
+    class Program
+    {
+        public const int SAIR = 0;
+        public const int LER_ARQUIVOS = 1;
+        public const int TABUADA = 2;
+        public const int CALCULO_MEDIA = 3;
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
