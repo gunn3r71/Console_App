0edb0b5b59b4bbc4a18c0d3b81bb301465038b60
Lucas <lucasgunner3@gmail.com>
2021-04-21 18:05:35 -0300

    Estruturando Menu

--- ProgramacaoFuncionalDotNet/Program.cs
+++ ProgramacaoFuncionalDotNet/Program.cs
@@ -11,7 +11,33 @@ namespace ProgramacaoFuncionalDotNet
 
         static void Main(string[] args)
         {
-            
+            while (true)
+            {
+                string menu = "Olá Usuário, seja bem vindo ao nosso programa!!!\n\n"
+                    + "Agora digite uma das opções abaixo: \n\n"
+                    + $"Para Ler Arquivos, Digite {LER_ARQUIVOS};\n"
+                    +$"Para Calcular uma tabuada, Digite {TABUADA};\n"
+                    +$"Para Efetuar um cálcuo de média, Digite {CALCULO_MEDIA};\n\n";
+
+                Console.WriteLine($"{menu}Digite {SAIR} para sair do programa!");
+                Console.Write("> ");
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado por utilizar nosso programa!!!");
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
