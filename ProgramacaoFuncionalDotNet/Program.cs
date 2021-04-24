125cc42bafef125860e2f5cac40f8e35ccf2e7c1
Lucas <lucasgunner3@gmail.com>
2021-04-23 22:43:34 -0300

    Rollback na classe program

--- ProgramacaoFuncionalDotNet/Program.cs
+++ ProgramacaoFuncionalDotNet/Program.cs
@@ -10,10 +10,6 @@ namespace ProgramacaoFuncionalDotNet
         static void Main(string[] args)
         {
             //Menu.Criar();
-
-            var c = new Cliente();
-            c.AdicionarCliente();
-
             var clientes = Cliente.LerClientes();
 
             foreach(var cliente in clientes)
