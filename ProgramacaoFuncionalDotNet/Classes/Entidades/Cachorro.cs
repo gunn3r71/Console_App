e5f210734d2a7935d80c116deca20d9b1135aacc
Lucas <lucas.p.oliveira@opah.com.br>
2021-04-29 23:19:18 -0300

    properties

--- ProgramacaoFuncionalDotNet/Classes/Entidades/Cachorro.cs
+++ ProgramacaoFuncionalDotNet/Classes/Entidades/Cachorro.cs
@@ -4,6 +4,18 @@ namespace Classes.Entidades
 {
     class Cachorro : AnimalAbstract
     {
+        public int idade; //propriedade simples
+        private string nomeDono = "Lucas";
+
+        public string Raca { get; set; } //propriedade complexa
+        public string NomeDono //propriedade complexa somente com get   
+        { 
+            get 
+            {
+                return nomeDono;           
+            }
+        }
+
         public override string Comunicar()
         {
             return "Latindo";
